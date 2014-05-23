13ce2c16bbf544771912509a0d4b4b85cbb8bd46
William Langford <langfordwill1@gmail.com>
2014-05-23 09:29:29 +0100

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	$(".box").one("mouseover", function() {
 		$(".box").addClass('keyframeRun');
-		$(".country").fadeOut('slow', function() {
+		$(".country").fadeOut("3s");
+		$(selector).fadeOut('slow', function() {
 			$(".country").text("China");
 			$(".country").fadeIn("3s");
 		});
