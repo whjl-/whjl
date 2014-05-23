659486df3f128d6aeaac617ca35e9fb68841dd52
William Langford <langfordwill1@gmail.com>
2014-05-23 10:01:12 +0100

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
-	$(".box").one("mouseover", function() {
-		$(".box").addClass('keyframeRun');
-		$(".country").fadeOut("3s");
-		$(".country").fadeOut('slow', function() {
-			$(".country").text("Switzerland");
-			$(".country").fadeIn("3s");
-		});
-		$(".cross").addClass('keyframeRunIn');
+	$(".cross").one("mouseover", function() {
+		$(".cross").addClass('keyframeRun');
 	});
 });
